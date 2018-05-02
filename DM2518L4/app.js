42c583292f6239731e566ecd58ba7768c0eafd37
victorhv <victorhv@kth.se>
2018-05-03 01:01:38 +0200

    Networking working

--- DM2518L4/app.js
+++ DM2518L4/app.js
@@ -9,22 +9,30 @@ var app = {}
 var host = 'mqtt.evothings.com'
 var port = 1884
 
+var messageBoard;
+var inputBoard;
+var connectButton;
+var sendButton;
+var nickname;
+
+
 app.connected = false
 app.ready = false
+
 
 app.uuid = getUUID()
 
 function getUUID () {
-  if (window.isCordovaApp) {
-    var uuid = device.uuid
-    if ((uuid.length) > 16) {
-      // On iOS we get a uuid that is too long, strip it down to 16
-      uuid = uuid.substring(uuid.length - 16, uuid.length)
+    if (window.isCordovaApp) {
+        var uuid = device.uuid
+        if ((uuid.length) > 16) {
+            // On iOS we get a uuid that is too long, strip it down to 16
+            uuid = uuid.substring(uuid.length - 16, uuid.length)
+        }
+        return uuid
+    } else {
+        return guid()
     }
-    return uuid
-  } else {
-    return guid()
-  }
 }
 
 /**
@@ -35,11 +43,11 @@ function getUUID () {
  * @link http://slavik.meltser.info/?p=142
  */
 function guid () {
-  function _p8 (s) {
-    var p = (Math.random().toString(16) + '000000000').substr(2, 8)
-    return s ? '-' + p.substr(0, 4) + '-' + p.substr(4, 4) : p
-  }
-  return _p8() + _p8(true) + _p8(true) + _p8()
+    function _p8 (s) {
+        var p = (Math.random().toString(16) + '000000000').substr(2, 8)
+        return s ? '-' + p.substr(0, 4) + '-' + p.substr(4, 4) : p
+    }
+    return _p8() + _p8(true) + _p8(true) + _p8()
 }
 
 /*
@@ -54,130 +62,142 @@ app.generateColor = function (uuid) {
 */
 
 app.initialize = function () {
-  document.addEventListener(
-    'deviceready',
-    app.onReady,
-    false)
+    document.addEventListener(
+        'deviceready',
+        app.onReady,
+        false)
 }
 
 app.onReady = function () {
-  if (!app.ready) {
-    //app.color = app.generateColor(app.uuid) // Generate our own color from UUID
-    app.pubTopic = 'victor/hipster/chatroom/' + app.uuid + '/evt' // We publish to our own device topic
-    app.subTopic = 'victor/hipster/chatroom/+/evt' // We subscribe to all devices using "+" wildcard
-    app.setupMessageBoard()
-    app.setupInputBoard()
-    app.setupConnection()
-    app.ready = true
-  }
+    if (!app.ready) {
+        //app.color = app.generateColor(app.uuid) // Generate our own color from UUID
+        app.pubTopic = 'virrepinnen/' + app.uuid + '/evt' // We publish to our own device topic
+        app.subTopic = 'virrepinnen/+/evt' // We subscribe to all devices using "+" wildcard
+        app.setupMessageBoard()
+        app.setupInputBoard()
+        app.setupButtons()
+        app.ready = true
+    }
 }
 
 app.setupMessageBoard = function () {
-  var messageBoard = document.getElementById('message-container')
-  messageBoard.readOnly = true;
-
-
-  /*
-  app.ctx = canvas.getContext('2d')
-  var totalOffsetX = 0
-  var totalOffsetY = 0
-  var curElement = canvas
-  do {
-    totalOffsetX += curElement.offsetLeft
-    totalOffsetY += curElement.offsetTop
-  } while (curElement = curElement.offsetParent)
-  app.left = totalOffsetX
-  app.top = totalOffsetY
-
-  // We want to remember the beginning of the touch as app.pos
-  canvas.addEventListener('touchstart', function (event) {
-    // Found the following hack to make sure some
-    // Androids produce continuous touchmove events.
-    if (navigator.userAgent.match(/Android/i)) {
-      event.preventDefault()
-    }
-    var t = event.touches[0]
-    var x = Math.floor(t.clientX) - app.left
-    var y = Math.floor(t.clientY) - app.top
-    app.pos = {x: x, y: y}
-  })
-
-  // Then we publish a line from-to with our color and remember our app.pos
-  canvas.addEventListener('touchmove', function (event) {
-    var t = event.touches[0]
-    var x = Math.floor(t.clientX) - app.left
-    var y = Math.floor(t.clientY) - app.top
-    if (app.connected) {
-      var msg = JSON.stringify({from: app.pos, to: {x: x, y: y}, color: app.color})
-      app.publish(msg)
-    }
-    app.pos = {x: x, y: y}
-  })
-  */
+    // disable the background textarea
+    messageBoard = document.getElementById('message-background');
+    messageBoard.readOnly = true;
 }
 
 app.setupInputBoard = function() {
-
+    inputBoard = document.getElementById('input-board');
 }
+
+app.clearInputBoard = function() {
+    inputBoard.value = '';
+}
+
+app.setupButtons = function() {
+    connectButton = document.getElementById('connect');
+    sendButton = document.getElementById('send');
+
+    /* disable sendButton before connection */
+    sendButton.disabled = true;
+
+    // Add addEventListeners
+    connectButton.addEventListener("click", function(){
+        var name = inputBoard.value;
+        if(name !== '') {
+            nickname = name.trim();
+            app.setupConnection();
+            app.clearInputBoard();
+            connectButton.disabled = true;
+            sendButton.disabled = false;
+        }
+    });
+
+    sendButton.addEventListener("click", function(){
+        if(sendButton.disabled !== true) {
+            var text = inputBoard.value;
+            if(text !== '') {
+                var msg = {};
+                msg.name = nickname;
+                msg.text = text.trim();
+                app.sendMessage(msg);
+                app.clearInputBoard();
+            }
+        }
+    });
+
+    app.log("Type your name, then connect.")
+}
+
+app.sendMessage = function(msg) {
+    if(app.connected) {
+        var json = JSON.stringify({ name: msg.name, text: msg.text});
+        var message = new Paho.MQTT.Message(json)
+        message.destinationName = app.pubTopic
+        app.client.send(message)
+    } else {
+        app.log("You are not connected. Connect again.")
+    }
+}
+
+app.displayMessage = function(msg) {
+    messageBoard.value += msg.name + ": " + msg.text + "\n";
+}
+
+
 
 app.setupConnection = function () {
-  app.status('Connecting to ' + host + ':' + port + ' as ' + app.uuid)
-  app.client = new Paho.MQTT.Client(host, port, app.uuid)
-  app.client.onConnectionLost = app.onConnectionLost
-  app.client.onMessageArrived = app.onMessageArrived
-  var options = {
-    useSSL: true,
-    onSuccess: app.onConnect,
-    onFailure: app.onConnectFailure
-  }
-  app.client.connect(options)
-}
-
-app.publish = function (json) {
-  var message = new Paho.MQTT.Message(json)
-  message.destinationName = app.pubTopic
-  app.client.send(message)
+    app.client = new Paho.MQTT.Client(host, port, app.uuid)
+    app.client.onConnectionLost = app.onConnectionLost
+    app.client.onMessageArrived = app.onMessageArrived
+    var options = {
+        useSSL: true,
+        onSuccess: app.onConnect,
+        onFailure: app.onConnectFailure
+    }
+    app.client.connect(options)
 }
 
 app.subscribe = function () {
-  app.client.subscribe(app.subTopic)
-  console.log('Subscribed: ' + app.subTopic)
+    app.client.subscribe(app.subTopic)
+    console.log('Subscribed: ' + app.subTopic)
 }
 
 app.unsubscribe = function () {
-  app.client.unsubscribe(app.subTopic)
-  console.log('Unsubscribed: ' + app.subTopic)
+    app.client.unsubscribe(app.subTopic)
+    console.log('Unsubscribed: ' + app.subTopic)
 }
 
 app.onMessageArrived = function (message) {
-  var o = JSON.parse(message.payloadString)
-  app.ctx.beginPath()
-  app.ctx.moveTo(o.from.x, o.from.y)
-  app.ctx.lineTo(o.to.x, o.to.y)
-  app.ctx.strokeStyle = o.color
-  app.ctx.stroke()
+    var o = JSON.parse(message.payloadString);
+    var msg = {};
+    msg.name = o.name;
+    msg.text = o.text;
+    app.displayMessage(msg);
+    console.log("From: " + o.name + ", msg: " + o.text)
 }
 
 app.onConnect = function (context) {
-  app.subscribe()
-  app.status('Connected!')
-  app.connected = true
+    app.subscribe()
+    app.log("Connected as "+nickname);
+    app.connected = true
 }
 
 app.onConnectFailure = function (e) {
-  console.log('Failed to connect: ' + JSON.stringify(e))
+    console.log('Failed to connect: ' + JSON.stringify(e))
 }
 
 app.onConnectionLost = function (responseObject) {
-  app.status('Connection lost!')
-  console.log('Connection lost: ' + responseObject.errorMessage)
-  app.connected = false
+    console.log('Connection lost: ' + responseObject.errorMessage)
+    app.connected = false
 }
 
-app.status = function (s) {
-  console.log(s)
-  var info = document.getElementById('info')
-  info.innerHTML = s
+/* Log messaging */
+app.log = function(text) {
+    var msg = {};
+    msg.name = "Log";
+    msg.text = text;
+    app.displayMessage(msg);
 }
 
 app.initialize()
